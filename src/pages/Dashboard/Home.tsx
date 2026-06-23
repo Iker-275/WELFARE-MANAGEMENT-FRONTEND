f2eb03d8bfd0033a3dcc5b5a51ca8f1f42f7a9b8
Iker 275 <jeananderson275@gmail.com>
2026-06-24 01:02:31 +1000

    slight logo refactor

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -11,20 +11,20 @@ export default function Home() {
     <>
       <PageMeta title="Dashboard" description="Restaurant Dashboard" />
 
-      {/* <div className="grid grid-cols-12 gap-4 md:gap-6">
+      <div className="grid grid-cols-12 gap-4 md:gap-6">
         <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics data={dashboard} />
-          <SalesVsPaymentsChart data={dashboard} />
+          {/* <EcommerceMetrics data={dashboard} />
+          <SalesVsPaymentsChart data={dashboard} /> */}
         </div>
 
         <div className="col-span-12">
-          <RecentOrders orders={dashboard?.recentOrders || []} />
+          {/* <RecentOrders orders={dashboard?.recentOrders || []} /> */}
         </div>
 
         <div className="col-span-12">
-          <NewCustomers customers={dashboard?.newCustomers || []} />
+          {/* <NewCustomers customers={dashboard?.newCustomers || []} /> */}
         </div>
-      </div> */}
+      </div>
     </>
   );
 }
